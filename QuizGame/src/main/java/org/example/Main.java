f4b66e4b3373bdc2131c57fb7dfc529c49d7e34e
Eric Brink <ejb5154@gmail.com>
2023-12-03 17:54:27 -0500

    finished getAnswersToDisplay method

--- QuizGame/src/main/java/org/example/Main.java
+++ QuizGame/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Question question = new Question("test", "easy", "test", "Here is the question", "correct answer",
+                new String []{"incorrect 1", "incorrect 2", "incorrect 3"});
+
+        System.out.println(question.getQuestion());
+        System.out.println(Arrays.toString(question.getAnswersToDisplay()));
     }
 }
--- QuizGame/src/main/java/org/example/Question.java
+++ QuizGame/src/main/java/org/example/Question.java
@@ -1,6 +1,9 @@
 package org.example;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Question {
 
     private String type;
@@ -11,5 +14,94 @@ public class Question {
     private String correctAnswer;
     @JsonProperty("incorrect_answers")
     private String [] incorrectAnswers;
+    private String [] answersToDisplay;
 
+    public Question (String type, String difficulty, String category, String question, String correctAnswer, String[] incorrectAnswers) {
+        this.type = type;
+        this.difficulty = difficulty;
+        this.category = category;
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswers = incorrectAnswers;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public String[] getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void setIncorrectAnswers(String[] incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+    public String [] getAnswersToDisplay () {
+        String [] allAnswers = {getCorrectAnswer(), getIncorrectAnswers()[0], getIncorrectAnswers()[1], getIncorrectAnswers()[2]};
+        String [] answersToDisplay = new String[4];
+        int [] randIndexes = new int []{-1, -1, -1, -1};
+        Random random = new Random();
+        int randNum = -1;
+        boolean alreadyUsed;
+
+        for (int i = 0; i < 4; i++) {
+            alreadyUsed = false;
+
+            while (!alreadyUsed) {
+                randNum = random.nextInt(0, 4);
+
+                for (int n = 0; n < 4; n++) {
+                    if (randIndexes[n] == randNum) {
+                        break;
+                    } else if (n == 3) {
+                        alreadyUsed = true;
+                        break;
+                    }
+                }
+            }
+            randIndexes[i] = randNum;
+
+        }
+        for (int i = 0; i < 4; i++) {
+            answersToDisplay[i] = allAnswers[randIndexes[i]];
+        }
+        return answersToDisplay;
+
+    }
 }
