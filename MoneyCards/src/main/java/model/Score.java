e07d161982dc17a49f915902c980c345c20e8836
Eric Brink <ejb5154@gmail.com>
2024-01-16 13:47:36 -0500

    Added Score related classes

---
+++ MoneyCards/src/main/java/model/Score.java
@@ -0,0 +1,50 @@
+package model;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Score {
+
+    @NotNull
+    private int scoreId;
+    @Size (max = 3)
+    private String initials;
+    @NotNull
+    private int score;
+
+    public Score (){};
+
+    public Score(int scoreId, String initials, int score) {
+        this.scoreId = scoreId;
+        this.initials = initials;
+        this.score = score;
+    }
+
+    public int getScoreId() {
+        return scoreId;
+    }
+
+    public void setScoreId(int scoreId) {
+        this.scoreId = scoreId;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString () {
+        return scoreId + "  " + initials + "........................" + score;
+    }
+}
---
+++ MoneyCards/src/main/java/model/ScoreBoard.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+public class ScoreBoard {
+
+    private List<Score> scores;
+
+    public ScoreBoard () {};
+
+    public ScoreBoard (List<Score> scores) {
+        this.scores = scores;
+    }
+
+    public List<Score> getScores() {
+        return scores;
+    }
+
+    public void setScores(List<Score> scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    public String toString () {
+        String scoreBoardString =
+                "        HIGH SCORES        \n" +
+                "___________________________\n";
+        for (Score score: scores) {
+            scoreBoardString += score.toString() + "\n";
+        }
+        return scoreBoardString;
+    }
+}
