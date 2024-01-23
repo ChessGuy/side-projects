502ce5cbe2f62623f647ceb53853a74835d746ab
Eric Brink <ejb5154@gmail.com>
2024-01-23 11:59:38 -0500

    Working through ScoreDao and JdbcScoreDao

--- MoneyCards/src/main/java/dao/JdbcScoreDao.java
+++ MoneyCards/src/main/java/dao/JdbcScoreDao.java
@@ -1,4 +1,49 @@
 package dao;
 
-public class JdbcScoreDao {
+import model.Score;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.List;
+
+public class JdbcScoreDao implements ScoreDao{
+
+    JdbcTemplate jdbcTemplate;
+
+    public JdbcScoreDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private Score mapRowToScore(SqlRowSet rowSet) {
+        Score score = new Score();
+        score.setScoreId(rowSet.getInt("score_id"));
+        score.setInitials(rowSet.getString("initials"));
+        score.setScore(rowSet.getInt("score"));
+        return score;
+    }
+
+    @Override
+    public List<Score> getScores() {
+        return null;
+    }
+
+    @Override
+    public Score getScoreById() {
+        return null;
+    }
+
+    @Override
+    public Score createScore() {
+        return null;
+    }
+
+    @Override
+    public Score updateScore() {
+        return null;
+    }
+
+    @Override
+    public int deleteLowestScore() {
+        return 0;
+    }
 }
--- MoneyCards/src/main/java/dao/ScoreDao.java
+++ MoneyCards/src/main/java/dao/ScoreDao.java
@@ -1,6 +1,20 @@
 package dao;
 
+import model.Score;
+
+import java.util.List;
+
 public interface ScoreDao {
+
+    List<Score> getScores ();
+
+    Score getScoreById ();
+
+    Score createScore ();
+
+    Score updateScore ();
+
+    int deleteLowestScore ();
 
 
 }
