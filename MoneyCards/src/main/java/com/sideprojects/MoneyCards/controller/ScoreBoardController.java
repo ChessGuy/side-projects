499970c14c2942bfbc6c6e3797910e871bfff59c
Eric Brink <ejb5154@gmail.com>
2024-03-09 11:41:33 -0500

    Added create score and delete method to ScoreBoardController

--- MoneyCards/src/main/java/com/sideprojects/MoneyCards/controller/ScoreBoardController.java
+++ MoneyCards/src/main/java/com/sideprojects/MoneyCards/controller/ScoreBoardController.java
@@ -3,6 +3,7 @@ package com.sideprojects.MoneyCards.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sideprojects.MoneyCards.dao.ScoreDao;
 import com.sideprojects.MoneyCards.model.Score;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 
 @RestController
@@ -34,7 +38,12 @@ public class ScoreBoardController {
     }
 
     // Score createScore (Score score);
-
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/scores", method=RequestMethod.POST)
+    public Score add (@RequestParam Score score) {
+        return scoreDao.createScore(score);
+    }
+    
     // Score getLowestScore ();
     @RequestMapping(path = "/scores/lowest", method = RequestMethod.GET)
     public Score get (){
@@ -42,7 +51,11 @@ public class ScoreBoardController {
     }
 
     // int deleteLowestScore ();
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/scores", method = RequestMethod.DELETE)
+    public void delete (){
+        scoreDao.deleteLowestScore();
+    }
 
 
 }
