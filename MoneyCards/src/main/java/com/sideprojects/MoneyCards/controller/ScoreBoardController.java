8d88a7bbcda0848edbee2207dac8fae3b8e4605c
Eric Brink <ejb5154@gmail.com>
2024-03-14 15:02:12 -0400

    Fixed axios

--- MoneyCards/src/main/java/com/sideprojects/MoneyCards/controller/ScoreBoardController.java
+++ MoneyCards/src/main/java/com/sideprojects/MoneyCards/controller/ScoreBoardController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-
+@CrossOrigin
 @RestController
 public class ScoreBoardController {
 
