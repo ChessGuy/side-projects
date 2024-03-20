977c272b1cf46557a089ddac87eec16bfb91d812
Eric Brink <ejb5154@gmail.com>
2024-03-20 11:15:29 -0400

    Update to directory

---
+++ PianoMusic/PianoMusic/src/main/java/com/sideprojects/PianoMusic/PianoMusicApplication.java
@@ -0,0 +1,13 @@
+package com.sideprojects.PianoMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PianoMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PianoMusicApplication.class, args);
+	}
+
+}
---
+++ PianoMusic/PianoMusic/src/test/java/com/sideprojects/PianoMusic/PianoMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideprojects.PianoMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianoMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
